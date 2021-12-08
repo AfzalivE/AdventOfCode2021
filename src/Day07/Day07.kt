781ef632ce40668ee35fd5797d31e48ac314b22f
Afzal Najam <afzalnaj@gmail.com>
2021-12-07 23:27:13 -0500

    Complete Day 7, add Day 8 files

--- src/Day07/Day07.kt
+++ src/Day07/Day07.kt
@@ -1,22 +1,48 @@
-private const val DEBUG = true
+import kotlin.math.*
+
+private const val DEBUG = false
 
 fun main(vararg args: String) {
     setDebug(DEBUG)
 
+    fun calculateFuel1(target: Int, list: List<Int>): Int = list.sumOf {
+        abs(it - target)
+    }
+
+    fun calculateFuel2(target: Int, list: List<Int>): Int {
+        return list.sumOf {
+            val n = abs(it - target)
+            n * (n + 1) / 2
+        }
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val positions = input.first().split(",").map { it.toInt() }
+        val max = positions.maxOf { it }
+        val target = positions.sorted()[positions.size / 2]
+        debug(positions.sorted()[positions.size / 2])
+        debug(calculateFuel1(target, positions))
+        return calculateFuel1(target, positions)
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val positions = input.first().split(",").map { it.toInt() }
+        val avg = positions.average()
+        val roundedDownAvg = floor(avg).toInt()
+        val roundedUpAvg = ceil(avg).toInt()
+        val leastFuel = (roundedDownAvg..roundedUpAvg).minOf {
+            calculateFuel2(it, positions)
+        }
+        return leastFuel
     }
 
     val day = args[0]
     println(day)
-    
+
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("$day/${day}_test")
-    check(part1(testInput) == 6)
+    check(part1(testInput) == 37)
+    check(part2(testInput) == 168)
 
     val input = readInput("$day/${day}")
     println(part1(input))
---
+++ src/Day08/Day08.kt
@@ -0,0 +1,24 @@
+private const val DEBUG = true
+
+fun main(vararg args: String) {
+    setDebug(DEBUG)
+
+    fun part1(input: List<String>): Int {
+        return input.size
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+
+    val day = args[0]
+    println(day)
+    
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("$day/${day}_test")
+    check(part1(testInput) == 6)
+
+    val input = readInput("$day/${day}")
+    println(part1(input))
+    println(part2(input))
+}
